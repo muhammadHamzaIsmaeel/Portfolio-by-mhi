4547fe87196a8c4d7c6b012f658b258f36d7fe54
MuhammadHamzaSheikh6 <m.hamzashaikh6067@gmail.com>
2025-07-02 15:36:34 +0500

    update

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -7,7 +7,7 @@ import { apiVersion, dataset, projectId } from '../env'
 export const client = createClient({
   projectId,
   dataset,
-  token: process.env.SANITY_API_TOKEN,
+  token: process.env.NEXT_SANITY_API_TOKEN,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
